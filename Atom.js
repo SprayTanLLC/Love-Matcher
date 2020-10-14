7a709c44e4129be74f4f901a9c428d8a1a26457f
spraytanllc <spraytanllc@gmail.com>
2020-10-14 18:35:03 -0500

    add styles and bootstrap

--- Atom.js
+++ Atom.js
@@ -6,7 +6,12 @@ const getRandomNumber = () => {
 
 const replaceHTML = () => {
   document.getElementById("result").innerHTML =
-    getRandomNumber() + "% love potential";
+    "RESULT: " + getRandomNumber() + "% love potential";
+  document.getElementById("bs-butt").innerHTML = "Reset";
 };
 
-document.getElementById("clickMe").addEventListener("click", replaceHTML);
+//document.getElementById("clickMe").addEventListener("click", replaceHTML);
+
+document.getElementById("bs-butt").addEventListener("click", replaceHTML);
+
+// if the love compatability is under 50%, display a broken heart, if it is above 50%, display lots of hearts;
