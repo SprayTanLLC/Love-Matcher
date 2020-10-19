3cabdca31ad8a7bd00bb9c520c539220d3b31728
spraytanllc <spraytanllc@gmail.com>
2020-10-19 14:05:41 -0500

    add bootstrap design and reset button plus rest functionality

--- LoveMatcher.js
+++ LoveMatcher.js
@@ -1,17 +1,29 @@
-let randomNum = Math.floor(Math.random() * 100) + 1;
+let submitButton = document.getElementById("bs-butt");
+let resetButton = document.getElementById("clearForm");
+let loveForm = document.getElementById("loveForm");
+
 
 const getRandomNumber = () => {
+  let randomNum = Math.floor(Math.random() * 100) + 1;
   return randomNum;
 };
 
-const replaceHTML = () => {
+const handleSubmit = () => {
   document.getElementById("result").innerHTML =
-    "RESULT: " + getRandomNumber() + "% love potential";
-  document.getElementById("bs-butt").innerHTML = "Reset";
+     getRandomNumber() + "% love potential";
+  submitButton.style.display = "none";
+  resetButton.style.display = "inline-block";
 };
 
-//document.getElementById("clickMe").addEventListener("click", replaceHTML);
+const handleReset = () => {
+  loveForm.reset();
+  submitButton.style.display = "inline-block";
+  resetButton.style.display = "none";
+  document.getElementById("result").innerHTML = "RESULT: "
+}
 
-document.getElementById("bs-butt").addEventListener("click", replaceHTML);
+document.getElementById("bs-butt").addEventListener("click", handleSubmit);
+resetButton.addEventListener("click", handleReset);
+
 
 // if the love compatability is under 50%, display a broken heart, if it is above 50%, display lots of hearts;
